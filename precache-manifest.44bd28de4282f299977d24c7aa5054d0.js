9649b938e23d796608a2db0b056fda3424cddc53
lukmanhidayah01@gmail.com <lukmanhidayah01@gmail.com>
2020-05-15 05:33:22 +0700

    Updates

---
+++ precache-manifest.44bd28de4282f299977d24c7aa5054d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8321b5c458d6333f95fe1725c49ed8c1",
+    "url": "/lukmanhidayah/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80038693f28f68c69ef6",
+    "url": "/lukmanhidayah/monsters-rolodex/static/css/main.ff2a5998.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/lukmanhidayah/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lukmanhidayah/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80038693f28f68c69ef6",
+    "url": "/lukmanhidayah/monsters-rolodex/static/js/main.6d5b410f.chunk.js"
+  },
+  {
+    "revision": "70b065eef718120ad3e4",
+    "url": "/lukmanhidayah/monsters-rolodex/static/js/runtime-main.b25aac18.js"
+  }
+]);
